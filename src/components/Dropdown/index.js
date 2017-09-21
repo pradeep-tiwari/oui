fd68a723f5bdf9b7eda260eca74f9430f1e40674
inaki.aranzadi@optimizely.com <inaki.aranzadi@optimizely.com>
2017-09-21 11:29:24 -0700

    Delete classname prop

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -6,7 +6,6 @@ class Dropdown extends React.Component {
   render() {
     const {
       children,
-      classname,
       handleClick,
       icon = 'chevron',
       isDisabled = false,
@@ -14,10 +13,8 @@ class Dropdown extends React.Component {
       fullWidth,
       style,
       text,
-      width,
+      width = 200,
     } = this.props;
-
-    const mainClasses = classNames('lego-dropdown-group', classname);
 
     const buttonClass = classNames(
       'oui-button',
@@ -37,7 +34,7 @@ class Dropdown extends React.Component {
       <div
         data-ui-component={ true }
         ref='dropdown'
-        className={ mainClasses }
+        className='ego-dropdown-group'
         style={{ width: width }}>
         <button
           className={ buttonClass }
@@ -57,7 +54,6 @@ class Dropdown extends React.Component {
 
 Dropdown.propTypes = {
   children: PropTypes.node.isRequired,
-  classname: PropTypes.string,
   fullWidth: PropTypes.bool,
   handleClick: PropTypes.func,
   icon: PropTypes.oneOf([
