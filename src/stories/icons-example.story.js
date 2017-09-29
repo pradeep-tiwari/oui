81c648894f12cbb269c3b1f6782c96b8e6218d22
Dave Rau <dave.rau@optimizely.com>
2017-09-29 16:34:53 -0700

    Lint fixes

--- src/stories/icons-example.story.js
+++ src/stories/icons-example.story.js
@@ -15,53 +15,54 @@ stories
   .addDecorator(withKnobs);
 
 stories.add('all icons', () => {
-        const iconsElements = icons.map( (icon, index) => {
-          const Box = styled.div`
-          border: 1px dashed #ccc;
-          border-radius: 8px;
-          box-sizing: border-box;
-          display: flex;
-          height: 100px;
-          justify-content: center;
-          margin: 5px;
-          padding: 20px 10px
-          text-align: center;
-          width: 100px;
-          &:after {
-            color: white;
-            transition: all 0.4s ease;
-            content: '${icon.title}';
-            font-family: Helvetica;
-            font-size: 0.5rem;
-            max-width: 90px;
-            position: absolute;
-            margin-top: 70px;
-          }
-          &:hover:after {
-            color: #666;
-          }
-        `
-          return <Box key={index} style={ styles.iconBox }>
-            <Icon
-              style={styles.icon}
-              description={icon.title}
-              name={icon.title.split(' ').join('-')}
-              fill={ text('color', 'black') }
-              size={ select('size', {small: 'small', medium: 'medium', large: 'large'},'medium') }
-            />
-          </Box>
-        });
-        return (
-          <div className="App">
-            <Container>
-              { iconsElements }
-            </Container>
-          </div>
-        );
-      }
+  const iconsElements = icons.map((icon, index) => {
+    const Box = styled.div`
+    border: 1px dashed #ccc;
+    border-radius: 8px;
+    box-sizing: border-box;
+    display: flex;
+    height: 100px;
+    justify-content: center;
+    margin: 5px;
+    padding: 20px 10px
+    text-align: center;
+    width: 100px;
+    &:after {
+      color: white;
+      transition: all 0.4s ease;
+      content: '${icon.title}';
+      font-family: Helvetica;
+      font-size: 0.5rem;
+      max-width: 90px;
+      position: absolute;
+      margin-top: 70px;
+    }
+    &:hover:after {
+      color: #666;
+    }
+  `;
+    return (
+      <Box key={ index } style={ styles.iconBox }>
+        <Icon
+          style={ styles.icon }
+          description={ icon.title }
+          name={ icon.title.split(' ').join('-') }
+          fill={ text('color', 'black') }
+          size={ select('size', {small: 'small', medium: 'medium', large: 'large'}, 'medium') }
+        />
+      </Box>
+    );
+  });
+  return (
+    <div className="App">
+      <Container>
+        { iconsElements }
+      </Container>
+    </div>
   );
+});
 
-stories.add('icon docs', withInfo('react <Icon /> component')( () => {
+stories.add('icon docs', withInfo('react <Icon /> component')(() => {
   return (
     <Icon name='bell' size='large' />
   );
@@ -78,10 +79,10 @@ const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
-`
+`;
 
 const styles = {
   icon: {
-    alignSelf: "center",
-  }
-}
+    alignSelf: 'center',
+  },
+};
