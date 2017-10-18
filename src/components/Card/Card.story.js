41b161ee232feee4bcb55e43a1ea0a7310a7e67d
Jack Schlotthauer <jack.schlotthauer@optimizely.com>
2017-10-18 11:53:19 -0700

    updated story book

--- src/components/Card/Card.story.js
+++ src/components/Card/Card.story.js
@@ -1,17 +1,10 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text, select } from '@storybook/addon-knobs';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 import { withInfo } from '@storybook/addon-info';
 
 import Card from '../Card';
-
-const typeOptions = {
-  'bad-news': 'bad-news',
-  'brand': 'brand',
-  'good-news': 'good-news',
-  'warning': 'warning',
-};
 
 const stories = storiesOf('Card', module);
 stories
@@ -26,10 +19,10 @@ stories
   .add('card states', withInfo()(() => {
     return (
       <Card
-        title="Hello World"
-        testSection="card"
-        type={ select('type', typeOptions, 'warning') } >
-        { text('content', 'Hello! This is a short card example.') }
+        title={ text('title', 'Card Title') }
+        onClose={ boolean('onClose', false) }
+        testSection="card">
+        { text('body', 'Hello! This is a short card example.') }
       </Card>
     );
   }));
