16dfec5ae82047b0642db82f7e04e5afe9719f3e
inaki.aranzadi@optimizely.com <inaki.aranzadi@optimizely.com>
2017-11-29 22:53:23 -0800

    Fix Disclose test

--- src/components/Disclose/tests/index.js
+++ src/components/Disclose/tests/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Disclose from '../index';
 
-const disclose = shallow(<Disclose>un disclose</Disclose>);
+const disclose = shallow(<Disclose title='some title'>un disclose</Disclose>);
 
-describe('Disclose Component', () => {
+describe('Disclose Component ', () => {
   it('renders correctly', () => {
     expect(disclose).toMatchSnapshot();
   });
