8b40d880a0795de181076559288a398800028d0c
inaki.aranzadi@optimizely.com <inaki.aranzadi@optimizely.com>
2018-01-03 10:39:50 -0800

    Popper withoug OUI styles story

--- src/components/Popper/Popper.story.js
+++ src/components/Popper/Popper.story.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text, select } from '@storybook/addon-knobs';
 import { withInfo } from '@storybook/addon-info';
 
-import Popper from './index.js';
+import { Manager, Popper, Target, Arrow } from 'react-popper';
 
 const stories = storiesOf('Popper', module);
 
@@ -12,18 +13,67 @@ const stories = storiesOf('Popper', module);
 stories
   .addDecorator(withKnobs);
 
+const options = { left: 'left', right: 'right', top: 'top', bottom: 'bottom', 'left-start': 'left-start', 'right-start': 'right-start', 'top-start': 'top-start', 'bottom-start': 'bottom-start', 'left-end': 'left-end', 'right-end': 'right-end', 'top-end': 'top-end', 'bottom-end': 'bottom-end'};
 stories.add('default', withInfo('react <Popper /> component')(() => {
   return (
-    <Popper arrow>
-      <div style={{ width: 500, height: 500, background: 'pink'}}>
-        <div className='icon'>
-          this is the target element
-        </div>
-      </div>
-      <div style={{ width: 100, height: 55, background: 'grey' }}>
-        this is the popper element
-      </div>
-    </Popper>
+    <Container>
+      <SubContainer>
+        <ScrollContainer>
+          <StyledManager>
+            <StyledTarget >
+              StyledTarget Box
+            </StyledTarget>
+            <StyledPopper placement={`${select('placement', options, 'left')}`} className="popper">
+              Left Content
+              <Arrow className="popper__arrow"/>
+            </StyledPopper>
+          </StyledManager>
+        </ScrollContainer>
+      </SubContainer>
+    </Container>
   );
 }
 ));
+
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  height: 100vh;
+  background: #f6f6f6;
+`;
+
+const SubContainer = styled.div`
+  width: 400px;
+  height: 400px;
+  background: #666;
+  margin: 100px auto;
+  overflow: scroll;
+`;
+
+const ScrollContainer = styled.div`
+  width: 1000px;
+  height: 1000px;
+`;
+
+const StyledManager = styled(Manager)`
+  width: 150px;
+  height: 120px;
+  margin-top: 225px;
+  margin-left: 225px;
+  border: 1px dashed white;
+`;
+
+const StyledTarget = styled(Target)`
+  height: 100%;
+  background: pink;
+`;
+
+const StyledPopper = styled(Popper)`
+  background: paleturquoise;
+  height: 40px;
+  padding: 10px;
+  margin-right: 10px;
+  margin-left: 10px;
+`;
+
