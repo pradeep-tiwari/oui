b6ecd93bb97321fda1a3c8c1b18ed8e9847e1f34
Dave Rau <dave.rau@optimizely.com>
2018-07-11 17:15:49 -0700

    Add dashboard and header story examples for tabnav

--- src/components/TabNav/TabNav.story.js
+++ src/components/TabNav/TabNav.story.js
@@ -17,7 +17,68 @@ stories
   ));
 
 stories
-  .add('basic tabs', withInfo()(() => (<div>
+  .add('Dashboard tabs', withInfo()(() => (
+    <div className="root-wrap">
+      <div className="wrap">
+        <ul className="root-nav root-nav--open" data-test-section="p13n-root-navbar">
+          <li className="root-nav__logo push-double--bottom">
+            <a href="/v2/projects">
+              <div className="root-nav__logo--mark"></div>
+              <div className="root-nav__logo--full"></div>
+            </a>
+          </li>
+          <li className="push-double--ends">
+            <ul>
+              <li data-test-section="personalization-nav-tab">
+                <a href="/v2/projects/4576166120" className="root-nav__link root-nav__link--primary is-active">
+                  <svg className="root-nav__icon root-nav__icon--large">
+                  </svg>
+                  <div className="root-nav__link__text">
+                    Rootnav Item
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <div className="root-panel">
+          <div className="stage">
+            <div className="stage__item__content--column position--relative">
+              <div className="stage">
+                <div className="stage__item">
+                  <div className="stage__item__content--column">
+                    <div className="stage__item__content--column">
+                      <div className="push-quad--sides push-double--top flex--none" data-test-section="campaigns-root">
+                        <div className="muted" data-test-section="campaigns-root-project-name">Project Name</div>
+                        <div className="beta push--bottom">Experiments</div>
+
+                          <TabNav activeTab="second" style={ ['dashboard'] }>
+                            <TabNav.Tab onClick={ action('first tab clicked') } tabId="first">
+                              Tab #1
+                            </TabNav.Tab>
+                            <TabNav.Tab onClick={ action('second tab clicked') } tabId="second">
+                              Tab #2
+                            </TabNav.Tab>
+                            <TabNav.Tab onClick={ action('third tab clicked') } tabId="third">
+                              Tab #3
+                            </TabNav.Tab>
+                          </TabNav>
+
+                        </div>
+                        <div className="flex flex--1" data-test-section="layers-table-container">
+                          <div className="flex flex--1 flex--column"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )))
+  .add('Basic tabs', withInfo()(() => (
     <TabNav activeTab="first">
       <TabNav.Tab onClick={ action('first tab clicked') } tabId="first">
         Tab #1
@@ -29,8 +90,8 @@ stories
         Tab #3
       </TabNav.Tab>
     </TabNav>
-  </div>)))
-  .add('closed tabs', withInfo()(() => (<div>
+  )))
+  .add('Closed tabs', withInfo()(() => (
     <TabNav activeTab="second" style={ ['small'] }>
       <TabNav.Tab onClick={ action('first tab clicked') } tabId="first">
         Tab #1
@@ -42,8 +103,8 @@ stories
         Tab #3
       </TabNav.Tab>
     </TabNav>
-  </div>)))
-  .add('centered tabs', withInfo()(() => (<div>
+  )))
+  .add('Centered tabs', withInfo()(() => (
     <TabNav activeTab="first" style={ ['small', 'center'] }>
       <TabNav.Tab onClick={ action('first tab clicked') } tabId="first">
         Tab #1
@@ -55,8 +116,8 @@ stories
         Tab #3
       </TabNav.Tab>
     </TabNav>
-  </div>)))
-  .add('plain tabs', withInfo()(() => (<div>
+  )))
+  .add('Plain tabs', withInfo()(() => (
     <TabNav activeTab="first" style={ ['small', 'sub'] }>
       <TabNav.Tab onClick={ action('first tab clicked') } tabId="first">
         Tab #1
@@ -68,5 +129,17 @@ stories
         Tab #3
       </TabNav.Tab>
     </TabNav>
-  </div>)));
-
+  )))
+  .add('Header tabs', withInfo()(() => (
+    <TabNav activeTab="first" style={ ['header'] }>
+      <TabNav.Tab onClick={ action('first tab clicked') } tabId="first">
+        Tab #1
+      </TabNav.Tab>
+      <TabNav.Tab onClick={ action('second tab clicked') } tabId="second">
+        Tab #2
+      </TabNav.Tab>
+      <TabNav.Tab onClick={ action('third tab clicked') } tabId="third">
+        Tab #3
+      </TabNav.Tab>
+    </TabNav>
+  )));
